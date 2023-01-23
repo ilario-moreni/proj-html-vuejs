12a29ad31e244b5d330fe0a603e87d87425c719e
ilario-moreni <ilario2001.mor@gmail.com>
2023-01-23 13:25:18 +0100

    header styling (first processing)

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -11,19 +11,19 @@ export default {
 </script>
 
 <template lang="">
-    <div class="AppHeader container py-5">
+    <div class="AppHeader container py-4 px-0">
         <!-- left-side header -->
-        <div>
-            <img src="/src/assets/img/construction_logo.png" alt="">
+        <div class="header_logo_container">
+            <img src="/src/assets/img/construction_logo_retina.png" alt="">
         </div>
         <!-- right-side header -->
         <div class="rightside_header">
             <!-- sections -->
-            <div>
+            <div class="mx-4">
                 <ul class="header_sections_ul">
-                    <li v-for="item in store.header_sections" class="p-3">
+                    <li v-for="item in store.header_sections" class="p-4">
                         <div>
-                            <a :href="item.url">
+                            <a :href="item.url" :class="item.active ? 'activeSection' : ''">
                                 {{ item.label }}
                             </a>
                         </div>
@@ -32,7 +32,7 @@ export default {
             </div>
             <!-- get quote button -->
             <div>
-                <button type="button" class="btn bg-warning rounded-0">GET QUOTE</button>
+                <button type="button" class="btn rounded-0 py-2 px-4" id="get_quote_button">GET QUOTE</button>
             </div>
         </div>
     </div>
@@ -49,15 +49,31 @@ export default {
         justify-content: space-between;
     }
 
+    .header_logo_container{
+        img{
+            width: 190px;
+        }
+    }
+
 
     .rightside_header{
         display: flex;
+        align-items: center;
+
     }
 
     .header_sections_ul{
         @include mixin.ul_flex_unstyled;
         text-transform: uppercase;
+        font-weight: 500;
     }
 
-    
+    .activeSection{
+        color: $avada_yellow!important ;
+    }
+
+    #get_quote_button{
+        font-weight: 500;
+        background-color: $avada_yellow;
+    }
 </style>
